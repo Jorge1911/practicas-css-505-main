2a3c204ce0772be5b4c06d5de14cf9fd24595105
Jorge1911 <pollo.19xoco@gmail.com>
2022-10-13 08:07:23 -0500

    actualizacion 1.6

--- 13_breaking/app.js
+++ 13_breaking/app.js
@@ -1,10 +1,11 @@
-    const obtenerFrase=async()=>{
-    const api = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
-    const json=await api.json();
-    console.log(json)
+const tu = async () =>{
+    const api= await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const frase=await api.json();
+    let f=frase[0];
+    let divFrase=document.querySelector("#frase");
+    divFrase.innerHTML=`
+    <h1>${f.quote}</h1>
+    <p>-${f.author}</p>
 
-    let frase=document.querySelector("#frase");
-    frase.innerHTML=`
-    <h1>${json[0].quote}</h1>
-    <p>-${json[0].author}</p>`;
+    `;
 }
